9b145840adcc9acaadf9177c1b5f20cf31e9f6f3
axtk <axtkx@inbox.ru>
2025-06-22 02:09:30 +0300

    lint

--- src/UnpackedURLSchema.ts
+++ src/UnpackedURLSchema.ts
@@ -10,19 +10,22 @@ export type UnpackedURLSchema<T extends URLSchema> = T extends null
             params: UnpackedSchema<NonNullable<T>['params']>;
             query: UnpackedSchema<NonNullable<T>['query']>;
         }
-    : NonNullable<T> extends {params?: R | undefined; query: R}
-      ? {
-            params?: UnpackedSchema<NonNullable<T>['params']>;
-            query: UnpackedSchema<NonNullable<T>['query']>;
-        }
-    : NonNullable<T> extends {params: R; query?: R | undefined}
-      ? {
-            params: UnpackedSchema<NonNullable<T>['params']>;
-            query?: UnpackedSchema<NonNullable<T>['query']>;
-        }
-    : NonNullable<T> extends {params?: R | undefined; query?: R | undefined}
-      ? {
-            params?: UnpackedSchema<NonNullable<T>['params']>;
-            query?: UnpackedSchema<NonNullable<T>['query']>;
-        }
-      : undefined;
+      : NonNullable<T> extends {params?: R | undefined; query: R}
+        ? {
+              params?: UnpackedSchema<NonNullable<T>['params']>;
+              query: UnpackedSchema<NonNullable<T>['query']>;
+          }
+        : NonNullable<T> extends {params: R; query?: R | undefined}
+          ? {
+                params: UnpackedSchema<NonNullable<T>['params']>;
+                query?: UnpackedSchema<NonNullable<T>['query']>;
+            }
+          : NonNullable<T> extends {
+                  params?: R | undefined;
+                  query?: R | undefined;
+              }
+            ? {
+                  params?: UnpackedSchema<NonNullable<T>['params']>;
+                  query?: UnpackedSchema<NonNullable<T>['query']>;
+              }
+            : undefined;
